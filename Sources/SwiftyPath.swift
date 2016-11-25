e9e556cd88dc9a4d97a5732d3406a43750a14698
Kare Morstol <kare@nottoobadsoftware.com>
2016-11-25 22:53:32 +0100

    Add URL and string properties, 'exists' methods, and general cleanup.

--- Sources/SwiftyPath.swift
+++ Sources/SwiftyPath.swift
@@ -5,31 +5,70 @@ let Files = FileManager.default
 
 public protocol Path: CustomStringConvertible {
 	var components: [String] { get }
-	var relativestart: Array<String>.Index? { get }
+	var _relativestart: Array<String>.Index? { get }
 	static var separator: String { get }
+	static var home: DirectoryPath { get }
 }
 
 extension Path {
 	public static var separator: String { return "/" }
 
-	static var current: DirectoryPath {
+	public static var current: DirectoryPath {
 		return DirectoryPath(Files.currentDirectoryPath)
 	}
 
-	static var home: DirectoryPath {
+	public static var home: DirectoryPath {
 		return DirectoryPath(NSHomeDirectoryForUser(NSUserName())!)
 	}
 
-	public var root: DirectoryPath {
-		return DirectoryPath("/")
+	public static var root: DirectoryPath {
+		return DirectoryPath(DirectoryPath.separator)
+	}
+
+	public var isDirectory: Bool {
+		return self is DirectoryPath
+	}
+
+	public var string: String {
+		return DirectoryPath.separator + components.joined(separator: DirectoryPath.separator)
 	}
 
 	public var description: String {
-		return DirectoryPath.separator + components.dropFirst().joined(separator: DirectoryPath.separator)
+		return string
+	}
+
+	public var baseString: String? {
+		return _relativestart.map { DirectoryPath.separator +
+			components[components.startIndex..<$0].joined(separator: DirectoryPath.separator)
+		}
+	}
+
+	public var baseURL: URL? {
+		return baseString.map { URL(fileURLWithPath: $0, isDirectory: true) }
+	}
+
+	public var relativeString: String? {
+		return _relativestart.map {
+			components[$0..<components.endIndex].joined(separator: DirectoryPath.separator)
+		}
+	}
+
+	public var relativeURL: URL? {
+		return relativeString.map { URL(fileURLWithPath: $0, isDirectory: isDirectory) }
+	}
+
+	public var url: URL {
+		return URL(fileURLWithPath: relativeString ?? string, isDirectory: isDirectory, relativeTo: baseURL)
+	}
+
+	func exists() -> Bool {
+		return Files.fileExists(atPath: string)
 	}
 }
 
-func initPath <C: Collection>(_ c: C) -> ([String], Array<String>.Index?) where C.Iterator.Element == String, C.SubSequence: Collection, C.SubSequence.Iterator.Element == String {
+func initPath <C: Collection>(_ c: C) -> ([String], Array<String>.Index?)
+	where C.Iterator.Element == String, C.SubSequence: Collection, C.SubSequence.Iterator.Element == String {
+
 	let components: [String]
 	let relativestart: Array<String>.Index?
 	if c.first == "~" {
@@ -39,7 +78,7 @@ func initPath <C: Collection>(_ c: C) -> ([String], Array<String>.Index?) where 
 		components = DirectoryPath.current.components + c
 		relativestart = DirectoryPath.current.components.count
 	} else {
-		components = Array(c)
+		components = Array(c.dropFirst())
 		relativestart = nil
 	}
 	return (components, relativestart)
@@ -56,61 +95,75 @@ func initPath(_ string: String) -> ([String], Array<String>.Index?) {
 
 public struct DirectoryPath: Path {
 	public let components: [String]
-	public let relativestart: Array<String>.Index?
+	public let _relativestart: Array<String>.Index?
 
-	public init <C: Collection>(_ c: C) where C.Iterator.Element == String, C.SubSequence: Collection, C.SubSequence.Iterator.Element == String {
-		(components, relativestart) = initPath(c)
+	public init <C: Collection>(_ c: C)
+		where C.Iterator.Element == String, C.SubSequence: Collection, C.SubSequence.Iterator.Element == String {
+		(components, _relativestart) = initPath(c)
 	}
 
 	public init(_ string: String) {
-		(components, relativestart) = initPath(string)
+		(components, _relativestart) = initPath(string)
 	}
 }
 
 public struct FilePath: Path {
 	public let components: [String]
-	public let relativestart: Array<String>.Index?
+	public let _relativestart: Array<String>.Index?
 
-	public init <C: Collection>(_ c: C) where C.Iterator.Element == String, C.SubSequence: Collection, C.SubSequence.Iterator.Element == String {
+	public init <C: Collection>(_ c: C)
+		where C.Iterator.Element == String, C.SubSequence: Collection, C.SubSequence.Iterator.Element == String {
 		precondition(c.count > 0)
-		(components, relativestart) = initPath(c)
+		(components, _relativestart) = initPath(c)
+		precondition(components.last != "/", "A file path cannot end in a /. Use DirectoryPath for directories.")
 	}
 
 	public init(_ string: String) {
-		(components, relativestart) = initPath(string)
+		precondition(string.characters.last != "/", "A file path cannot end in a /. Use DirectoryPath for directories.")
+		(components, _relativestart) = initPath(string)
 	}
 }
 
 extension FilePath: ExpressibleByStringLiteral {
 	public init(extendedGraphemeClusterLiteral value: String) {
-		self = FilePath(value)
+		self.init(value)
 	}
 	public init(stringLiteral value: String) {
-		self = FilePath(value)
+		self.init(value)
 	}
 	public init(unicodeScalarLiteral value: String) {
-		self = FilePath(value)
+		self.init(value)
 	}
 }
 
 extension DirectoryPath: ExpressibleByStringLiteral {
 	public init(extendedGraphemeClusterLiteral value: String) {
-		self = DirectoryPath(value)
+		self.init(value)
 	}
 	public init(stringLiteral value: String) {
-		self = DirectoryPath(value)
+		self.init(value)
 	}
 	public init(unicodeScalarLiteral value: String) {
-		self = DirectoryPath(value)
+		self.init(value)
 	}
 }
 
 extension DirectoryPath {
 	public static func +(dir: DirectoryPath, file: FilePath) -> FilePath {
-		return FilePath(dir.components + file.components.suffix(from: file.relativestart ?? file.components.startIndex))
+		return FilePath([DirectoryPath.separator] + dir.components
+			+ file.components.suffix(from: file._relativestart ?? file.components.startIndex))
 	}
 
-	public static func +(dir: DirectoryPath, file: DirectoryPath) -> DirectoryPath {
-		return DirectoryPath(dir.components + file.components.suffix(from: file.relativestart ?? file.components.startIndex))
+	public static func +(leftdir: DirectoryPath, rightdir: DirectoryPath) -> DirectoryPath {
+		return DirectoryPath([DirectoryPath.separator] + leftdir.components
+			+ rightdir.components.suffix(from: rightdir._relativestart ?? rightdir.components.startIndex))
+	}
+
+	public func exists(filepath: FilePath) -> Bool {
+		return Files.fileExists(atPath: (self + filepath).string)
+	}
+
+	public func exists(filepath: String) -> Bool {
+		return Files.fileExists(atPath: (self + FilePath(string)).string)
 	}
 }
--- Tests/SwiftyPathTests/SwiftyPathTests.swift
+++ Tests/SwiftyPathTests/SwiftyPathTests.swift
@@ -1,18 +1,39 @@
 
 import XCTest
 import SwiftyPath
+import Foundation
 
 class SwiftyPathTests: XCTestCase {
-	func testExample() {
+	func testAddURLs() {
 		let folder: DirectoryPath = "/tmp"
-		print(folder)
-		XCTAssertEqual(String(describing: folder + "file"), "/tmp/file")
+		XCTAssertEqual(String(describing: folder + FilePath("file")), "/tmp/file")
 	}
 
+	func testRelativeFileURL() {
+		let filepath = FilePath("folder1/file1.txt")
+
+		XCTAssertFalse(filepath.isDirectory)
+		XCTAssertEqual(filepath.baseString, FileManager.default.currentDirectoryPath)
+		XCTAssertEqual(filepath.baseURL, FilePath.current.url)
+		XCTAssertEqual(filepath.relativeString, "folder1/file1.txt")
+		XCTAssertEqual(filepath.relativeURL, URL(fileURLWithPath: "folder1/file1.txt", isDirectory: false))
+		XCTAssertEqual(filepath.string, FileManager.default.currentDirectoryPath + "/folder1/file1.txt")
+	}
+
+	func testRelativeDirectoryURL() {
+		let directorypath = DirectoryPath("directory1/directory2")
+
+		XCTAssertTrue(directorypath.isDirectory)
+		XCTAssertEqual(directorypath.baseString, FileManager.default.currentDirectoryPath)
+		XCTAssertEqual(directorypath.baseURL, FilePath.current.url)
+		XCTAssertEqual(directorypath.relativeString, "directory1/directory2")
+		XCTAssertEqual(directorypath.relativeURL, URL(fileURLWithPath: "directory1/directory2", isDirectory: true))
+		XCTAssertEqual(directorypath.string, FileManager.default.currentDirectoryPath + "/directory1/directory2")
+	}
 
 	static var allTests : [(String, (SwiftyPathTests) -> () throws -> Void)] {
 		return [
-			("testExample", testExample),
+			("testAddURLs", testAddURLs),
 		]
 	}
 }
