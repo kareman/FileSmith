55841cee0adef51725facae2a97bdeeb1aed229c
Kare Morstol <kare@nottoobadsoftware.com>
2017-01-10 01:47:20 +0100

    Make all associated types of FileSystemError Path.

--- Sources/Errors.swift
+++ Sources/Errors.swift
@@ -9,8 +9,8 @@
 public enum FileSystemError: Error {
 	case alreadyExists(path: Path)
 	case notFound(path: Path)
-	case isDirectory(path: DirectoryPath)
-	case notDirectory(path: FilePath)
+	case isDirectory(path: Path)
+	case notDirectory(path: Path)
 	case invalidAccess(path: Path, writing: Bool)
 	case couldNotCreate(path: Path)
 	case outsideSandbox(path: Path)
