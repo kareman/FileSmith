a7dc2b5b81e05582600dfe90871c0bcc8570cba1
Kare Morstol <kare@nottoobadsoftware.com>
2018-04-12 03:19:05 +0200

    Update to Swift 4.1 and SwiftShell 4.1.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,22 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "FileSmith"
+	name: "FileSmith",
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/kareman/SwiftShell.git", from: "4.1.0-beta.1")
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "FileSmith",
+			dependencies: ["SwiftShell"]),
+		.testTarget(
+			name: "FileSmithTests",
+			dependencies: ["FileSmith"]),
+		]
 )
-
-package.dependencies.append(.Package(url: "https://github.com/kareman/SwiftShell.git", majorVersion: 3))
